e2597fb5e9b09363d76acbc649ff954ba66eabbf
RaduTudorIon <radutudorion@gmail.com>
2020-06-23 23:33:13 +0300

    Custom validation

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto
+    [CourseTitleMustBeDifferentFromDescription(ErrorMessage = "Title must be different from description")]
+    public class CourseForCreationDto //: IValidatableObject
     {
+        [Required(ErrorMessage = "You should fill out a title")]
+        [MaxLength(100, ErrorMessage = "The title should be up to 100 characters")]
         public string Title { get; set; }
+        [MaxLength(100)]
         public string Description { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "The provided description should be different from the title",
+        //           new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -4,6 +4,8 @@ using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,7 +26,54 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           services.AddControllers();
+           services.AddControllers()
+                .ConfigureApiBehaviorOptions(setupAction =>
+                {
+                    setupAction.InvalidModelStateResponseFactory = context =>
+                    {
+                        //create a problem details object
+                        var problemDetailsFactory = context.HttpContext.RequestServices
+                        .GetRequiredService<ProblemDetailsFactory>();
+
+                        var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+                            context.HttpContext,
+                            context.ModelState);
+
+                        //add aditional info not added by default
+                        problemDetails.Detail = "See the errors field for details";
+                        problemDetails.Instance = context.HttpContext.Request.Path;
+
+                        //find out which status code to use
+                        var actionExecutingContext =
+                            context as Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext;
+
+                        //if there are modelstate errors & all arguments were correctly
+                        //found/parsed we're dealing with validation errors
+                        if((context.ModelState.ErrorCount > 0) &&
+                        (actionExecutingContext?.ActionArguments.Count == 
+                        context.ActionDescriptor.Parameters.Count))
+                        {
+                            problemDetails.Type = "https://courselibrary.com/modelvalidationproblem";
+                            problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                            problemDetails.Title = "One or more validation errors occured";
+
+                            return new UnprocessableEntityObjectResult(problemDetails)
+                            {
+                                ContentTypes = {"application/problem+json"}
+                            };
+                        };
+
+                        //if one of the arguments wasn't correctly found / couldn't be parsed
+                        //we're dealing with null/unparseable input
+                        problemDetails.Status = StatusCodes.Status400BadRequest;
+                        problemDetails.Title = "One or more errors occured";
+                        return new UnprocessableEntityObjectResult(problemDetails)
+                        {
+                            ContentTypes = { "application/problem+json" }
+                        };
+
+                    };
+                });
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
              
